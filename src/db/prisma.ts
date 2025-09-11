d4429787008507e542052e10a29310c4642e4bb2
priyanshu-tiwariii <priyanshu2213008@akgec.ac.in>
2025-09-11 22:36:07 +0530

    feat(db) -> create middleware for the prisma client and attach to the context

---
+++ src/db/prisma.ts
@@ -0,0 +1,21 @@
+//middleware to create the prisma client and attach it to the context
+
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Context } from "hono";
+import { createMiddleware } from "hono/factory";
+
+export const prismaClient = createMiddleware( async(c:Context,next:any)=>{
+    if(!c.get("prisma")){
+    const prisma = new PrismaClient({
+        datasources:{
+            db:{
+                url:c.env.DATABASE_URL
+            },
+        }
+    }).$extends(withAccelerate());
+    c.set("prisma", prisma);
+}
+    await next();
+});
+
